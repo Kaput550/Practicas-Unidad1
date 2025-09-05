5e675d819ebcb3f27098dfaea403ba914c109e49
Kaput550 <jisaacgm7@gmail.com>
2025-09-05 12:23:17 -0600

    Commit inicial, cambiar color

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -21,7 +21,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.practica1.ui.theme.Practica1Theme
-
+//Comentario
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
