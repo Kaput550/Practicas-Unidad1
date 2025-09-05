86dc1481b1b5a8459a803648ce1ebf24625b8a9a
Kaput550 <jisaacgm7@gmail.com>
2025-09-05 12:11:11 -0600

    Commit inicial, cambiar color

---
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.practica1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.practica1.ui.theme.Practica1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Practica1Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    GreetingText(
+                        name = "Jonathan Garcia",
+                        ctrl = "22130806"
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Practica1Theme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun GreetingText(name:String, ctrl: String, modifier: Modifier = Modifier){
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,)
+    {
+        Text(
+            text = name,
+            fontSize = 80.sp,
+            lineHeight = 70.sp,
+            textAlign = TextAlign.Center,
+            color = Color.Magenta
+        )
+
+        Text (
+            text = ctrl,
+            fontSize = 40.sp,
+            color =  Color.Blue
+        )
+
+    }
+}
